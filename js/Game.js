a7a8f41229eea2691ca8fe0d5804bd5c259c7876
AnnikaWHJ <mishr10333@gapps.uwcsea.edu.sg>
2020-12-29 16:16:34 +0800

    Added images

--- js/Game.js
+++ js/Game.js
@@ -29,10 +29,15 @@ class Game {
       form.display();
     }
 
+
     car1 = createSprite(100,200);
+    car1.addImage('car1',carImg1)
     car2 = createSprite(300,200);
+    car2.addImage('car2',carImg2)
     car3 = createSprite(500,200);
+    car3.addImage('car3',carImg3)
     car4 = createSprite(700,200);
+    car4.addImage('car4',carImg4)
     cars = [car1, car2, car3, car4];
   }
 
@@ -42,13 +47,15 @@ class Game {
     Player.getPlayerInfo();
     
     if(allPlayers !== undefined){
+      background("#C68767")
+      image(trackImg,0,-displayHeight*4,displayWidth,displayHeight*5)
       //var display_position = 100;
       
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 175;
       var y;
 
       for(var plr in allPlayers){
@@ -79,6 +86,12 @@ class Game {
       player.update();
     }
 
+    if(player.distance > 3860){
+  gameState = 2
+    }
     drawSprites();
+  }
+  end(){
+    console.log("Game Ended")
   }
 }
